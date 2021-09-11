db9915889021b3883bbc2d0f5b59c6383ce964de
wizbeans <75798911+wizbeans@users.noreply.github.com>
2021-09-11 19:12:32 +0530

    Add files via upload

---
+++ whatsapp_responses.py
@@ -0,0 +1,17 @@
+def response(input_message):
+    message = input_message.lower()
+
+    if message == 'nice':
+        return 'Very nice'
+    elif message == 'hello':
+        return 'Hello there'
+    elif message == 'hi':
+        return 'hey'
+    elif message == 'sup':
+        return 'nm'
+    elif message == 'thanks':
+        return 'np:)'
+    elif message == 'how are you':
+        return 'fine and u?'
+    else:
+        return 'Cool!'
